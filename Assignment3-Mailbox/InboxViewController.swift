4a7eb974d89e4366eadbf980975b32c873646cbc
Salyards, Adey <adey.salyards@capitalone.com>
2015-11-15 19:43:08 -0800

    v2
    Redid the messageView gesture and it’s working way better. Woo!

--- Assignment3-Mailbox/InboxViewController.swift
+++ Assignment3-Mailbox/InboxViewController.swift
@@ -26,6 +26,9 @@ class InboxViewController: UIViewController {
     var grayColor: UIColor!
     var greenColor: UIColor!
     var redColor: UIColor!
+    var yellowColor: UIColor!
+    var brownColor: UIColor!
+    
     var initialCenter: CGPoint!
     var initialCenterArchiveButton: CGPoint!
     var initialCenterLaterButton: CGPoint!
@@ -40,7 +43,9 @@ class InboxViewController: UIViewController {
         redColor = UIColor(red: 235/255, green: 84/255, blue: 51/255, alpha: 1)
         grayColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1)
         greenColor = UIColor(red: 112/255, green: 216/255, blue: 98/255, alpha: 1)
-        originalCenter = messageView.center
+        yellowColor = UIColor(red: 250/227, green: 211/227, blue: 51/227, alpha: 1)
+        brownColor = UIColor(red: 216/255, green: 165/255, blue: 117/255, alpha: 1)
+        
         scrollView.contentSize = CGSize(width: 320, height: 1000)
         // Do any additional setup after loading the view.
     }
@@ -62,102 +67,78 @@ class InboxViewController: UIViewController {
         
         fakeView.backgroundColor = UIColor.redColor()
     }
-
-    @IBAction func didPanMessage(sender: UIPanGestureRecognizer) {
+    
+    
+    @IBAction func didPanMessageGesture(sender: UIPanGestureRecognizer) {
+        
         let location = sender.locationInView(view)
         let translation = sender.translationInView(view)
-        //let alphaTranslation = abs(translation.x / 320)
         
         if sender.state == UIGestureRecognizerState.Began {
-            
-            archiveButton.alpha = 1
-            laterButton.alpha = 1
             
             self.initialCenter = messageView.center
             self.initialCenterArchiveButton = archiveButton.center
             self.initialCenterLaterButton = laterButton.center
             
         }else if sender.state == UIGestureRecognizerState.Changed {
-            messageView.center = CGPoint(x: initialCenter.x + translation.x , y: initialCenter.y)
             
-            let velocity = sender.velocityInView(view)
+            self.messageView.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y)
             
-            if velocity.x >= 0 {
+            if messageView.center.x >= 161 && messageView.center.x <= 261 {
                 //drag to the right
-                archiveButton.center = CGPoint(x: initialCenterArchiveButton.x + translation.x - 80, y: initialCenterArchiveButton.y)
-                laterView.alpha = 0
                 archiveView.alpha = 1
+                laterView.alpha = 0
+                archiveView.backgroundColor = grayColor
+                self.archiveButton.selected = false
+            }else if messageView.center.x >= 261 && messageView.center.x <= 411{
+               archiveView.backgroundColor = greenColor
+                self.archiveButton.selected = false
+                archiveButton.center = CGPoint(x: initialCenterArchiveButton.x + translation.x - 80, y: initialCenterArchiveButton.y)
+            }else if messageView.center.x >= 411 {
+                archiveView.backgroundColor = redColor
+                self.archiveButton.selected = true
+                archiveButton.center = CGPoint(x: initialCenterArchiveButton.x + translation.x - 80, y: initialCenterArchiveButton.y)
                 
-                if location.x <= 80 {
-                    self.archiveView.backgroundColor = grayColor
-                    
-                }else if location.x >= 80 && location.x <= 120 {
-                    let alpha = CGFloat(location.x)
-                    let variableAlpha = convertValue(alpha, r1Min: 0, r1Max: 320, r2Min: 0, r2Max: 1)
-                    self.archiveView.backgroundColor = UIColor(red: 112/255, green: 216/255, blue: 98/255, alpha: variableAlpha)
-                    //green
-                    
-                }else if location.x >= 120 && location.x <= 250 {
-                    self.archiveView.backgroundColor = greenColor
-                    
-                } else if location.x >= 250 {
-                    self.archiveView.backgroundColor = redColor
-                    self.archiveButton.selected = true
-                }
-                
-            } else if velocity.x <= 0 {
+            }else if messageView.center.x <= 161 && messageView.center.x >= 61 {
                 //drag to the left
-                laterView.alpha = 1
                 archiveView.alpha = 0
+                laterView.alpha = 1
+                laterView.backgroundColor = grayColor
+                self.laterButton.selected = false
+            }else if messageView.center.x <= 61 && messageView.center.x >= -89 {
+                laterView.backgroundColor = yellowColor
                 laterButton.center = CGPoint(x: initialCenterLaterButton.x + translation.x + 80, y: initialCenterLaterButton.y)
-                
-                if location.x >= 220 {
-                    self.laterView.backgroundColor = UIColor(red: 227/255, green: 227/255, blue: 227/255, alpha: 1)
-                    //gray
-                    
-                } else if location.x <= 200 && location.x >= 40 {
-                    self.laterView.backgroundColor = UIColor(red: 250/227, green: 211/227, blue: 51/227, alpha: 1)
-                    //yellow
-                    
-                } else if location.x <= 40 {
-                    self.laterView.backgroundColor = UIColor(red: 216/255, green: 165/255, blue: 117/255, alpha: 1)
-                    self.laterButton.selected = true
-                    //brown
-                    
-                }
+                self.laterButton.selected = false
+            }else if messageView.center.x <= -89 {
+                laterButton.center = CGPoint(x: initialCenterLaterButton.x + translation.x + 80, y: initialCenterLaterButton.y)
+                laterView.backgroundColor = brownColor
+                self.laterButton.selected = true
             }
             
         }else if sender.state == UIGestureRecognizerState.Ended {
             
-            archiveButton.alpha = 0
-            laterButton.alpha = 0
-            
-            archiveView.alpha = 1
-            laterView.alpha = 1
-            
             if location.x >= 60 {
                 UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 20, options: [], animations: { () -> Void in
-                    self.messageView.center = self.originalCenter
+                    self.messageView.center = self.initialCenter
                     }, completion: { (Bool) -> Void in
                 })
                 
             } else if location.x <= 60 && location.x >= 40 {
                 UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 20, options: [], animations: { () -> Void in
-                    self.messageView.center = self.originalCenter
+                    self.messageView.center = self.initialCenter
                     }, completion: { (Bool) -> Void in
                 })
                 
             } else if location.x <= 40 {
                 performSegueWithIdentifier("rescheduleSegue", sender: nil)
                 UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 20, options: [], animations: { () -> Void in
-                    self.messageView.center = self.originalCenter
+                    self.messageView.center = self.initialCenter
                     }, completion: { (Bool) -> Void in
                 })
             }
         }
-
     }
-    
+
     @IBAction func didTapHamburgerButton(sender: AnyObject) {
         
         if hamburgerButton.selected == false {
@@ -186,7 +167,7 @@ class InboxViewController: UIViewController {
             
             if velocity.x <= 0 {
                 UIView.animateWithDuration(0.5, delay: 0, options: [], animations: { () -> Void in
-                    self.inboxView.center.x = CGFloat(self.originalCenter.x)
+                    self.inboxView.center.x = CGFloat(self.initialCenter.x)
                     }, completion: { (Bool) -> Void in
                 })
                 messageViewPanGesture.enabled = true
@@ -219,3 +200,4 @@ class InboxViewController: UIViewController {
     */
 
 }
+
